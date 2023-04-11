02e65cdd7160c485a32b7035f5ad41c889e759fe
mbastudent099 <130301319+mbastudent099@users.noreply.github.com>
2023-04-11 07:27:17 +0530

    Update 16oop.cpp

--- OOPS LAb/16oop.cpp
+++ OOPS LAb/16oop.cpp
@@ -1,30 +1,30 @@
 #include<iostream>
 using namespace std;
 
-// forward declaration of class B
+
 class B;
 
-// class A
+
 class A{
     int num;
 public:
     void setNum(int n){
         num = n;
     }
-    friend int findGreatest(A, B); // friend function declaration
+    friend int findGreatest(A, B); 
 };
 
-// class B
+
 class B{
     int num;
 public:
     void setNum(int n){
         num = n;
     }
-    friend int findGreatest(A, B); // friend function declaration
+    friend int findGreatest(A, B); 
 };
 
-// friend function definition
+
 int findGreatest(A objA, B objB){
     if(objA.num > objB.num){
         return objA.num;
@@ -38,14 +38,14 @@ int main(){
     A objA;
     B objB;
 
-    // set values
+    
     objA.setNum(10);
     objB.setNum(20);
 
-    // call friend function
+    
     int greatest = findGreatest(objA, objB);
 
-    // display result
+    
     cout<<"The greatest number is "<<greatest<<endl;
 
     return 0;
